734e0af0e78f351059482927f62599ed5af01273
methodoti <marcelo@patricio.net.br>
2025-07-05 13:17:37 -0300

    Made adjustments to the code.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // this is the week01 Exercise2
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
     }
 }
