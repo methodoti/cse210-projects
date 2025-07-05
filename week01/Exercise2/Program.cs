6dddbb57a8602d3a608522029c5434ceeb14664e
methodoti <marcelo@patricio.net.br>
2025-07-05 13:40:24 -0300

    Back to normal on some files

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // this is the week01 Exercise2
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
     }
 }
